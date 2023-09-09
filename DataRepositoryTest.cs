e27dc1628a03d6a10ddee9d5b92d544c38b4483e
angelochecked <angi99552@gmail.com>
2023-09-09 19:22:52 +0200

    fix test

--- DataRepositoryTest.cs
+++ DataRepositoryTest.cs
@@ -17,7 +17,7 @@ public class DataRepositoryTests
         var data = dataRepository.GetData();
 
         Assert.That(data.data?.contents?[0], Is.EqualTo(
-            new Content(false, false, "Deep Sax 1.mp3", "MS On-Demand H.264", "01:02:53", "828c3395-0303-45cd-9651-bd7226b3198a", false, "test video 23", false, false, 4, "15fa3bc1-760d-45bb-9eeb-1508d3a9a81d", "GgNXuUOo0n1d", 11, 1, false, DateTime.Parse("2021-04-14T21:01:14.533"), DateTime.Parse("2021-04-14T21:05:42.81"), DateTime.Parse("2021-04-14T21:01:14.533"), 1, "https://picsum.photos/seed/picsum/600/600")));
+            new Content(false, false, "Deep Sax 1.mp3", "MS On-Demand H.264", "01:02:53", "828c3395-0303-45cd-9651-bd7226b3198a", false, "test video 23", false, true, 4, "15fa3bc1-760d-45bb-9eeb-1508d3a9a81d", "GgNXuUOo0n1d", 11, 1, false, DateTime.Parse("2021-04-14T21:01:14.533"), DateTime.Parse("2021-04-14T21:05:42.81"), DateTime.Parse("2021-04-14T21:01:14.533"), 1, "https://picsum.photos/seed/picsum/600/600")));
     }
 
     [Test]
